45a8f1ee16c6a48cf71b38e4db529f3f295ae021
kirkmonz <kirkemmons2@gmail.com>
2023-09-05 14:29:12 -0400

    update laptimer, implement new start, tick, and createLap function

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template lang="pug">
+
   v-container
     v-row.mt-5(
       justify="center"
@@ -60,7 +61,7 @@
           no-data-text="No laps have been recorded"
         )
           template(#item.time="{ item: lap }")
-            span {{ $dayjs.duration(lap.time, 'milliseconds').format('mm:ss:sss') }}
+            span {{ formatLapTime(lap.time) }}
           template(#item.remove="{ item: lap }")
             v-btn(
               icon
@@ -71,25 +72,6 @@
                 color="red"
                 size="large"
               ) mdi-trash-can-outline
-
-    //- v-row(
-    //-   justify="center"
-    //- )
-    //-   v-col(
-    //-     cols="9"
-    //-     lg="6"
-    //-   )
-    //-     v-card.mt-4
-    //-       v-sparkline(
-    //-         show-labels
-    //-         label-size="6"
-    //-         :value="lapTimes"
-    //-         :gradient="['#1feaea', '#f72047', '#ffd200']"
-    //-         line-width="3"
-    //-         auto-draw
-    //-         stroke-linecap="square"
-    //-         class="elevation-18"
-    //-       )
 
     v-row.mb-1(
     )
@@ -108,6 +90,7 @@
 </template>
 
 <script>
+
 import {} from 'vuex'
 import { makeFindMixin } from 'feathers-vuex'
 
@@ -126,10 +109,15 @@ export default {
         }
       ],
       timerState: 'stopped',
-      currentTimer: 0,
-      time: '00:00:00',
+      time: '00:00:00:000',
       ticker: undefined,
-      latestLap: ''
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      milliseconds: 0,
+      latestLap: 0,
+      totalElapsedTime: 0
+
     }
   },
 
@@ -146,23 +134,50 @@ export default {
 
     start () {
       if (this.timerState !== 'running') {
-        this.tick()
+        if (this.timerState === 'paused') {
+          // Calculate the time difference between the current total elapsed time and the latest lap time
+          const timeDifference = this.totalElapsedTime - this.latestLap
+
+          // Update the latest lap time to the current total elapsed time minus the time difference
+          this.latestLap = this.totalElapsedTime - timeDifference
+        } else {
+          // If the timer is not paused, set the latest lap time to the current total elapsed time
+          this.latestLap = this.totalElapsedTime
+        }
+        this.startTimer() // Call startTimer to initiate the timer
         this.timerState = 'running'
       }
     },
 
-    // The tick function increments the currentTimer variable by 1 every 10 milliseconds and updates the time variable with the current time in the HH:mm:ss format using the formatTime function.
-    tick () {
-      this.ticker = setInterval(() => {
-        this.currentTimer += 10
-        this.time = this.formatTime(this.currentTimer)
-      }, 10)
+    startTimer () {
+      this.ticker = setInterval(this.tick, 10) // Call tick every 10 milliseconds
     },
 
-    // The formatTime function takes the number of seconds passed as an argument and uses the dayjs library to create a new dayjs object representing the current time (measuredTime), adds the number of seconds passed to it and returns the resulting time in the HH:mm:ss format.
-    formatTime (milliseconds) {
-      const measuredTime = this.$dayjs.duration(milliseconds, 'milliseconds')
-      return measuredTime.format('mm:ss:sss')
+    formatLapTime (time) {
+      const lapDuration = this.$dayjs.duration(time, 'milliseconds')
+      return lapDuration.format('HH:mm:ss:SSS')
+    },
+
+    // The tick function increments the currentTimer variable by 1 every 10 milliseconds and updates the time variable with the current time in the HH:mm:ss format using the formatTime function.
+    tick () {
+      this.milliseconds += 10
+      this.seconds += Math.floor(this.milliseconds / 1000)
+      this.minutes += Math.floor(this.seconds / 60)
+      this.hours += Math.floor(this.minutes / 60)
+
+      this.milliseconds %= 1000
+      this.seconds %= 60
+      this.minutes %= 60
+      this.hours %= 24 // Optionally, reset hours after 24 hours
+
+      this.totalElapsedTime = this.hours * 60 * 60 * 1000 + this.minutes * 60 * 1000 + this.seconds * 1000 + this.milliseconds
+
+      const h = String(this.hours).padStart(2, '0')
+      const m = String(this.minutes).padStart(2, '0')
+      const s = String(this.seconds).padStart(2, '0')
+      const ms = String(this.milliseconds).padStart(3, '0')
+
+      this.time = `${h}:${m}:${s}:${ms}`
     },
 
     async createLap () {
@@ -171,11 +186,7 @@ export default {
         return
       }
 
-      const lapStartTime = this.latestLap !== '' ? this.latestLap : 0
-      // console.log(lapStartTime)
-      const lapEndTime = this.currentTimer // Store the current time as the end time of the lap
-
-      const lapTimeDuration = (lapEndTime - lapStartTime)
+      const lapTimeDuration = this.totalElapsedTime - (this.latestLap || 0)
 
       console.log(lapTimeDuration)
 
@@ -186,7 +197,7 @@ export default {
 
       await lap.create()
 
-      this.latestLap = lapEndTime
+      this.latestLap = this.totalElapsedTime
     },
 
     async fetchLaps () {
@@ -210,26 +221,34 @@ export default {
 
     // The stop function stops the timer by clearing the interval that was set with the setInterval function in the tick function and sets the timerState to 'paused'.
     stop () {
-      window.clearInterval(this.ticker)
-      this.timerState = 'paused'
+      clearInterval(this.ticker) // Clear the timer interval
+      this.timerState = 'paused' // Set the timer state to 'paused'
     },
 
     // The reset function stops the timer by clearing the interval that was set with the setInterval function in the tick function, sets the timerState to 'stopped', resets the time to '00:00:00', sets the currentTimer to 0, and empties the laps array.
     reset () {
       window.clearInterval(this.ticker)
       this.timerState = 'stopped'
-      this.time = '00:00:00'
-      this.currentTimer = 0
+      this.time = '00:00:00:000'
+      this.hours = 0 // Reset hours to 0
+      this.minutes = 0 // Reset minutes to 0
+      this.seconds = 0 // Reset seconds to 0
+      this.milliseconds = 0
       this.latestLap = 0
+
+      // Reset the totalElapsedTime to 0 when resetting the timer
+      this.totalElapsedTime = 0
 
       // Remove lap times from localStorage
       localStorage.removeItem('laps')
     }
   }
 }
+
 </script>
 
 <style lang="scss">
+
 .container {
   margin: 0 auto;
   background-color: #e0d5d5;
@@ -250,4 +269,5 @@ export default {
 .v-btn {
   border: thin solid black;
 }
+
 </style>
